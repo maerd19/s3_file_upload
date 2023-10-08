70d779064d8841ed426f436cad70b30e84515790
Ivan Alejandro Hernandez <revolutions_333@hotmail.com>
2023-10-08 15:27:38 -0600

    Add dotenv configuration for environment variable management

--- express-upload-backend/server.js
+++ express-upload-backend/server.js
@@ -2,9 +2,20 @@ const express = require('express');
 const multer = require('multer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const AWS = require('aws-sdk');
+require('dotenv').config();
 
 const app = express();
 const port = 3001;
+
+// Configure AWS
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+});
+
+const s3 = new AWS.S3();
 
 app.use(cors()); // Enable CORS for all routes
 app.use(bodyParser.json());
@@ -22,10 +33,23 @@ const upload = multer({
 });
 
 app.post('/upload', upload.single('file'), (req, res) => {
-    // Here, req.file contains the uploaded file data in memory
-    // You'd typically use an AWS SDK to upload the file to S3 from here
-    // Simulating a successful upload response for now
-    res.json({ status: "File received on server!" });
+    const file = req.file;
+
+    // Set up the payload to send the file to S3
+    const uploadParams = {
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: file.originalname, // Filename to save as on S3
+        Body: file.buffer,
+        ContentType: file.mimetype
+    };
+
+    s3.upload(uploadParams, function(err, data) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json({ status: "Success", url: data.Location });
+        }
+    });
 });
 
 app.listen(port, () => {
