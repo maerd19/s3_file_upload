7e7fb497902b5c75f91490e7a55258e8922f6a41
Ivan Alejandro Hernandez <revolutions_333@hotmail.com>
2023-10-09 10:21:24 -0600

    Add upload button

--- file-upload-frontend/src/components/FileUpload.js
+++ file-upload-frontend/src/components/FileUpload.js
@@ -11,40 +11,46 @@ import { boxStyles } from "../styles/FileUploadStyles";
 const FileUpload = ({ onFileUpload }) => {
   // State variable to keep track of uploaded files
   const [uploadedFiles, setUploadedFiles] = useState([]);
+  const [selectedFile, setSelectedFile] = useState(null);
 
   // Callback triggered when files are dropped or selected
-  const handleDrop = useCallback(
-    (event) => {
-      event.preventDefault();
+  const handleFileSelection = useCallback((event) => {
+    event.preventDefault();
 
-      // Get the files from the event
-      const files = event.dataTransfer
-        ? event.dataTransfer.files
-        : event.target.files;
+    // Get the files from the event
+    const files = event.dataTransfer
+      ? event.dataTransfer.files
+      : event.target.files;
 
-      // Check if the file size is valid
-      if (!isValidFileSize(files[0].size)) {
-        alert("You cannot upload files larger than 1MB.");
-        return;
-      }
+    // Check if the file size is valid
+    if (!isValidFileSize(files[0].size)) {
+      alert("You cannot upload files larger than 1MB.");
+      return;
+    }
 
-      // Validate file extension & MIME type
-      if (
-        !isValidFileExtension(files[0].name) ||
-        !isValidMimeType(files[0].type)
-      ) {
-        alert("Invalid file type or extension.");
-        return;
-      }
+    // Validate file extension & MIME type
+    if (
+      !isValidFileExtension(files[0].name) ||
+      !isValidMimeType(files[0].type)
+    ) {
+      alert("Invalid file type or extension.");
+      return;
+    }
 
-      // Add the file to the uploaded files list
-      setUploadedFiles([...uploadedFiles, files[0]]);
+    setSelectedFile(files[0]);
+    setUploadedFiles((prevFiles) => [...prevFiles, files[0]]);
+  }, []);
 
-      // Callback to parent component with the file
-      onFileUpload(files[0]);
-    },
-    [onFileUpload, uploadedFiles]
-  );
+  const handleUpload = useCallback(() => {
+    if (!selectedFile) {
+      alert("Please select a file first.");
+      return;
+    }
+
+    onFileUpload(selectedFile);
+    setSelectedFile(null);
+    setUploadedFiles([]);  // Clear the uploadedFiles list
+  }, [onFileUpload, selectedFile]);
 
   // Prevent default behavior of drag events to make the drop event work
   const handleDragOver = (event) => {
@@ -54,17 +60,23 @@ const FileUpload = ({ onFileUpload }) => {
   return (
     <Box
       sx={boxStyles}
-      onDrop={handleDrop}
+      onDrop={handleFileSelection}
       onDragOver={handleDragOver}
     >
       <Typography variant="body1">Drag & drop your file here or</Typography>
-      <Button
-        variant="contained"
-        component="label" // allows the button to encapsulate the input
-      >
+      <Button variant="contained" component="label">
         Click to select
-        <input type="file" hidden onChange={handleDrop} />
+        <input type="file" hidden onChange={handleFileSelection} />
       </Button>
+      {selectedFile && (
+        <Button
+          variant="contained"
+          onClick={handleUpload}
+          style={{ marginTop: "10px" }}
+        >
+          Upload
+        </Button>
+      )}
       {uploadedFiles.length > 0 && (
         <List>
           {uploadedFiles.map((file, index) => (
